77f43135eb553eb14a914c1396fd5ce5e950c535
JJ Lau <j.j.j.lau@live.com>
2017-05-04 15:02:36 +1000

    Got a QR scanner working

--- src/pages/points/points.ts
+++ src/pages/points/points.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Toast } from '@ionic-native/toast';
+// import { ZBar, ZBarOptions } from '@ionic-native/zbar';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 /**
  * Generated class for the Points page.
@@ -12,13 +14,13 @@ import { Toast } from '@ionic-native/toast';
 @Component({
   selector: 'page-points',
   templateUrl: 'points.html',
-  providers: [Toast]
+  providers: [Toast, BarcodeScanner]
 })
 export class Points {
 
   public selectedPoints: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: Toast) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: Toast, private barcodeScanner: BarcodeScanner) {
     this.selectedPoints = 20
   }
 
@@ -28,10 +30,30 @@ export class Points {
 
   distributePoints(): void {
     console.log('IT HASNT CRASHED');
-    this.toast.show('Im a toast', '5000', 'center').subscribe(
-      toast => {
-        console.log(toast);
-      }
-    );
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log(barcodeData.format);
+      console.log(barcodeData.cancelled);
+      console.log(barcodeData.text);
+    }, (err) => {
+      console.log(err);
+    });
+    // this.toast.show('Im a toast', '5000', 'center').subscribe(
+    //   toast => {
+    //     console.log(toast);
+    //   }
+    // );
+    //   let zBarOptions: ZBarOptions = {
+    //     flash: 'off',
+    //     drawSight: false
+    //   };
+    //
+    //   this.zbar.scan(zBarOptions)
+    //     .then(result => {
+    //       console.log(result); // Scanned code
+    //     })
+    //     .catch(error => {
+    //       console.log(error); // Error message
+    //     });
+    // }
   }
 }
